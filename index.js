69a717b9f50bb799f82a0f096785892835ca484d
tanveerat11 <tanveerahm11swe@gmail.com>
2023-01-29 21:58:54 +0600

    drums kit commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const snareDrum = document.getElementById("Snare").addEventListener("click",function(){
+
+    const playSnare = new Audio("Snare-Drum-Hit-Level-6b-www.fesliyanstudios.com.mp3")
+    playSnare.play();
+
+
+})
+
+const hiHatsDrum = document.getElementById("Hi-Hats").addEventListener("click",function(){
+
+    const playHiHats = new Audio("Hi-Hat-Open-Hit-B1-www.fesliyanstudios.com.mp3")
+    playHiHats.play();
+
+
+})
+
+
+const splashCymbalDrum = document.getElementById("Splash-Cymbal").addEventListener("click",function(){
+
+    const playSplashCymbal = new Audio("Splash-Cymbal-Hit-C-www.fesliyanstudios.com.mp3")
+    playSplashCymbal.play();
+
+
+})
+
+const mediumTomDrum = document.getElementById("Medium-Tom").addEventListener("click",function(){
+
+    const playMediumTom = new Audio("Medium-Tom-Drum-Hit-Level-7B-www.fesliyanstudios.com.mp3")
+    playMediumTom.play();
+
+
+})
+
+const rideCymbalDrum = document.getElementById("Ride-Cymbal").addEventListener("click",function(){
+
+    const playRideCymbal = new Audio("Ride-Cymbal-Metal-Slide-Scrape-E-www.fesliyanstudios.com.mp3")
+    playRideCymbal.play();
+
+
+})
+
+const bassDrum = document.getElementById("Bass").addEventListener("click",function(){
+
+    const playBass = new Audio("Bass-Drum-Hit-Level-6b-www.fesliyanstudios.com.mp3")
+    playBass.play();
+
+
+})
+
+
